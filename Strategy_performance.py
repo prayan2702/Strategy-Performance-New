b6ff04d7172777be94320a07fa50bc64c94d83e4
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-15 23:51:48 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -351,7 +351,7 @@ with col3:
     # Apply conditional formatting
     def color_positive_negative(val):
         """Style positive values green and negative values light red."""
-        color = '#caf1b0' if float(val) > 0 else '#fbd9d3'
+        color = '#caf1b0' if float(val) > 0 else '#FB3234'
         return f'background-color: {color}'
     
     
