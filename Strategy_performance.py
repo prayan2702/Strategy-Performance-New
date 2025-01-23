60ca968d5414c9fa7dc7ce80a057feed52164228
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-24 01:08:06 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -448,7 +448,7 @@ with col2:
           </div>
           <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js" async>
           {{
-            "width": "900",
+            "width": "550",
             "height": "900",
             "symbolsGroups": [
               {{
