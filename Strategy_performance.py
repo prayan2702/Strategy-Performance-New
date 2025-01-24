01347a65af63895d6cf10c5345bd33b00c641e0d
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-24 21:26:45 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -404,11 +404,7 @@ with col2:
         <!-- TradingView Widget BEGIN -->
         <div class="tradingview-widget-container">
           <div class="tradingview-widget-container__widget"></div>
-          <div class="tradingview-widget-copyright">
-            <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-              <span class="blue-text">Track all markets on TradingView</span>
-            </a>
-          </div>
+          <div class="tradingview-widget-copyright"></div> <!-- Removed "Track all markets on TradingView" -->
           <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
           {{
           "symbols": {symbols},
@@ -429,7 +425,7 @@ with col2:
           "fontSize": "10",
           "noTimeScale": false,
           "valuesTracking": "1",
-          "changeMode": "price-and-percent",
+          "changeMode": "price-and-percent",  <!-- Shows both price and percentage changes -->
           "chartType": "area",
           "maLineColor": "#2962FF",
           "maLineWidth": 1,
