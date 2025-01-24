d4026d415617fde314a2e085563183a0bc493aea
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-24 21:14:43 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -182,7 +182,7 @@ tradingview_widget = f"""
   <div class="tradingview-widget-container__widget"></div>
   <div class="tradingview-widget-copyright">
     <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
-      <span class="blue-text">Track all markets on TradingView</span>
+      # <span class="blue-text">Track all markets on TradingView</span>
     </a>
   </div>
   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
