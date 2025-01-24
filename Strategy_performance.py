f92734427042cb8bfaea40e92fb4196e638a1d25
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-24 23:52:04 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -453,11 +453,12 @@ with col2:
         st.warning("No stocks available for the symbol overview widget.")
 
 #*****************
-    widget_html = f"""
+    # Static TradingView widget HTML code
+    widget_html = """
     <!-- TradingView Widget BEGIN -->
     <div class="tradingview-widget-container">
       <div class="tradingview-widget-container__widget"></div>
-      <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+      <div class="tradingview-widget-copyright"></div>
       <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-tickers.js" async>
       {
       "symbols": [
@@ -482,11 +483,13 @@ with col2:
       "showSymbolLogo": true,
       "colorTheme": "light",
       "locale": "en"
-    }
+      }
       </script>
     </div>
     <!-- TradingView Widget END -->
-    # Embed the widget in Streamlit using markdown
+    """
+    
+    # Embed the widget in your Streamlit app using markdown
     st.markdown(widget_html, unsafe_allow_html=True)
 #**********************************************
 
