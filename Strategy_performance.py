9d1eaec14a093c02e6777d02e1873c65eab51e77
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-01-25 18:35:26 +0530

    Update Strategy_performance.py

--- Strategy_performance.py
+++ Strategy_performance.py
@@ -435,7 +435,7 @@ with col2:
                 <div class="tradingview-widget-copyright"></div>
                 <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
                 {{
-                    "width": "950",
+                    "width": "900",
                     "height": "610",
                     "symbol": "{selected_stock}",
                     "interval": "D",
